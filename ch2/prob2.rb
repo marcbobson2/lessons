3681f6c3445d6a8dbbf15875a1f6117979be162e
Marc Davis <marcbobson@gmail.com>
2015-07-17 00:26:28 +0000

    this is an update to a prior commit

--- ch2/prob2.rb
+++ ch2/prob2.rb
@@ -2,12 +2,14 @@ puts "how old are you?"
 age=gets.chomp.to_i
 
 for i in 1..4
-  newage=age + (i*10)
-  years=i*10
 
-
-puts "you will be #{newage} in #{years} years"
+puts "you will be #{age+(i*10)} in #{i*10} years"
 
   end
+puts "enter your name"
+name=gets.chomp
+
+
+10.times {|i| puts "#{i}.#{name}"}
 
 
