c440e1a403e5aa064b67bd4e4bb222685d69cac5
Marc Davis <marcbobson@gmail.com>
2015-08-30 16:02:05 +0000

    wow

---
+++ ch2_variables/problem4.rb
@@ -0,0 +1,7 @@
+puts "Enter your first name:"
+firstname=gets.chomp.to_s
+puts "Enter your last name:"
+lastname=gets.chomp.to_s
+
+puts "Welcome #{firstname + " " + lastname}. It's good to see you again."
+
